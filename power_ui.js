9c8c479ffe770cb5a97740685c009cd103b2714f
André Maia <pitymaia@gmail.com>
2018-10-08 23:35:26 -0300

    Add PowerMenus

--- power_ui.js
+++ power_ui.js
@@ -2,56 +2,83 @@
 
 'use strict';
 
-const power = {
-    ui: {
-        menuItemsById: {},
-        menus: [],
-        menuById: function (id) {
-            return power.ui.menus.find(menu => menu.id === id);
-        },
-        menuItemById: function (id) {
-            for (const menu of power.ui.menus) {
-                if (menu.itemsById[id]) {
-                    return menu.itemsById[id];
-                }
-            }
-        },
-        menuElById: function (id) {
-            return power.ui.menus.find(menu => menu.id === id).element;
-        },
-        menuElItemById: function (id) {
-            for (const menu of power.ui.menus) {
-                if (menu.itemsById[id]) {
-                    return menu.itemsById[id].element;
-                }
-            }
-        },
-    },
-    truth: {},
-};
-
-const menus = document.getElementsByClassName('power-menu');
-
-for (const menu of menus) {
-    const menuItems = menu.getElementsByClassName('power-menu-item');
-    const menuId = menu.getAttribute('id');
-    let menuToAdd = {element: menu, id: menuId, items: {elements: menuItems}, itemsById: {}};
-
-    for (const menuItem of menuItems) {
-        const menuItemId = menuItem.getAttribute('id');
-        if (menuItemId) {
-            menuToAdd.itemsById[menuItemId] = {id: menuItemId, element: menuItem};
-        }
+class Event {
+    constructor(name) {
+        this.observers = [];
+        if (name)  Event.index[name] = this;
     }
-    power.ui.menus.push(menuToAdd);
+    subscribe(fn, ctx) { // *ctx* is what *this* will be inside *fn*.
+        this.observers.push({fn, ctx});
+    }
+    unsubscribe(fn) {
+        this.observers = this.observers.filter((x) => x !== fn);
+    }
+    broadcast() { // Accepts arguments.
+        for (const o of this.observers)  o.fn.apply(o.ctx, arguments);
+    }
+}
+Event.index = {}; // storage for all named events
+
+class PowerUi {
+    constructor() {
+        this.menus = new PowerMenus();
+        this.ui = {};
+        this.truth = {};
+    }
 }
 
-power.ui.menuElItemById('news').addEventListener('click', function() {
-    window.console.log('Click news', power.ui.menus);
+class PowerMenus {
+    constructor() {
+        this.menus = [];
+
+        const menus = document.getElementsByClassName('power-menu');
+        for (const menu of menus) {
+            const menuItems = menu.getElementsByClassName('power-menu-item');
+            const menuId = menu.getAttribute('id');
+            let menuToAdd = {element: menu, id: menuId, items: []};
+
+            // Add all menuItems
+            for (const menuItem of menuItems) {
+                const menuItemId = menuItem.getAttribute('id') || null;
+                menuToAdd.items.push({element: menuItem, id: menuItemId});
+            }
+            this.menus.push(menuToAdd);
+        }
+    }
+    menuById(id) {
+        return this.menus.find(menu => menu.id === id);
+    }
+    menuItemById(id) {
+        for (const menu of this.menus) {
+            const menuItem = menu.items.find(item => item.id === id); // jshint ignore:line
+            if (menuItem) {
+                return menuItem;
+            }
+        }
+    }
+    menuElById(id) {
+        return this.menus.find(menu => menu.id === id).element;
+    }
+    menuItemElById(id) {
+        for (const menu of this.menus) {
+            const menuItem = menu.items.find(item => item.id === id); // jshint ignore:line
+            if (menuItem) {
+                return menuItem.element;
+            }
+        }
+    }
+}
+
+let app = new PowerUi();
+
+window.console.log('power', app);
+
+app.menus.menuItemElById('news').addEventListener('click', function() {
+    window.console.log('Click news', app.menus);
 });
-power.ui.menuElItemById('news').addEventListener('mouseover', function() {
+app.menus.menuItemElById('news').addEventListener('mouseover', function() {
     window.console.log('Hover news');
 });
-power.ui.menuElItemById('esporte').addEventListener('click', function() {
-    window.console.log('Click esporte');
+app.menus.menuItemElById('esporte').addEventListener('click', function() {
+    window.console.log('Click esporte', app);
 });
