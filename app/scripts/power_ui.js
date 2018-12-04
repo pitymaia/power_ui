883e0169ed972449905f52f13d2ed8d097437422
André Maia <pitymaia@gmail.com>
2018-12-04 13:50:59 -0200

    Remove dev tests from power_ui

--- app/scripts/power_ui.js
+++ app/scripts/power_ui.js
@@ -770,36 +770,35 @@ class PowerMenu {
 	}
 }
 
-
 class PwcPity extends PowCssHover {
-	constructor(element) {
-		super(element);
-		this.$_pwAttrName = 'data-pwc-pity';
-		this.element = element;
-		console.log('pwcPity is live!', this.$_pwAttrName);
-	}
+    constructor(element) {
+        super(element);
+        this.$_pwAttrName = 'data-pwc-pity';
+        this.element = element;
+        console.log('pwcPity is live!', this.$_pwAttrName);
+    }
 }
 const inject = [{name: 'data-pwc-pity', obj: PwcPity}];
 // TODO: testeExtendName is to find a way to modularize multiple exstensions
 // Make a way to this works as links of a chain
 // const testeExtendName = function () {return PowerMenu;};
 // class TesteMenu extends testeExtendName {
-// 	constructor(menu, info) {
-// 		super(menu);
-// 		this.testMenu = true;
-// 		this.descri = info.descri;
-// 	}
+//  constructor(menu, info) {
+//      super(menu);
+//      this.testMenu = true;
+//      this.descri = info.descri;
+//  }
 // }
 
 // class TesteUi extends PowerUi {
-// 	constructor() {
-// 		super(inject);
-// 		this.fake = {};
-// 	}
-// 	newPowerMenu(menu) {
-// 		const info = {descri: 'Descrição do menu'};
-// 		return new TesteMenu(menu, info);
-// 	}
+//  constructor() {
+//      super(inject);
+//      this.fake = {};
+//  }
+//  newPowerMenu(menu) {
+//      const info = {descri: 'Descrição do menu'};
+//      return new TesteMenu(menu, info);
+//  }
 // }
 // let app = new TesteUi(inject);
 let app = new PowerUi(inject);
