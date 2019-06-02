bbb048c68459429dbe6c745364ca9433f5164791
André Maia <pitymaia@gmail.com>
2019-06-02 17:49:24 -0300

    Refactor accordion section

--- app/scripts/power_ui.js
+++ app/scripts/power_ui.js
@@ -1030,16 +1030,15 @@ class AccordionModel {
 }
 
 
-class AccordionHtmlView {
+class AccordionSectionModel {
 	constructor(ctx) {
 		this.ctx = ctx;
-		this.childrenSections = ctx.getChildrenByPowerCss('powerAccordionSection');
-		this.childrenActions = ctx.getChildrenByPowerCss('powerAction');
+		this.active = false;
 	}
 }
 
 
-class AccordionHtmlCtrl {
+class AccordionSectionHtmlCtrl {
 	constructor(ctx) {
 		this.ctx = ctx;
 	}
@@ -1047,13 +1046,14 @@ class AccordionHtmlCtrl {
 	// Open and close the accordion panel
 	toggle() {
 		// If not allow multipleSectionsOpen, close the other sections
-		if (!this.ctx.model.multipleSectionsOpen) {
-			for (const action in this.ctx.htmlView.childrenActions) {
-				// Only closes if is not this section and if is active
-				const targetAction = this.ctx.htmlView.childrenActions[action];
-				if (targetAction.targetObj.id !== this.ctx.id && targetAction._$pwActive) {
+		if (!this.ctx.powerAccordion.model.multipleSectionsOpen) {
+			for (const action in this.ctx.powerAccordion.childrenActions) {
+				// Only toggle if is not this section and or is active
+				const targetAction = this.ctx.powerAccordion.childrenActions[action];
+				if ((targetAction.targetObj.id !== this.ctx.powerAccordion.id) && targetAction._$pwActive && (this.ctx !== targetAction.targetObj)) {
 					// This prevent the targetAction.toggle call this action again, so this flag avoid a loop to occurs
 					targetAction.toggle({avoidCallAction: true});
+					targetAction.targetObj.model.active = !targetAction.targetObj.model.active;
 				}
 			}
 		}
@@ -1068,8 +1068,8 @@ class PowerAccordion extends PowerTarget {
 	}
 
 	init() {
-		this.htmlView = new AccordionHtmlView(this);
-		this.htmlCtrl = new AccordionHtmlCtrl(this);
+		this.childrenSections = this.getChildrenByPowerCss('powerAccordionSection');
+		this.childrenActions = this.getChildrenByPowerCss('powerAction');
 	}
 }
 // Inject the power css on PowerUi
@@ -1079,9 +1079,12 @@ PowerUi.injectPowerCss({name: 'power-accordion'});
 class PowerAccordionSection extends PowerTarget {
 	constructor(element) {
 		super(element);
+		this.model = new AccordionSectionModel(this);
+		this.ctrl = 'htmlCtrl'; // default controller
 	}
 
 	init() {
+		this.htmlCtrl = new AccordionSectionHtmlCtrl(this);
 		let parent = this.parent;
 		// Add the accordion to this powerSection
 		do {
@@ -1094,7 +1097,7 @@ class PowerAccordionSection extends PowerTarget {
 	}
 
 	action() {
-		this.powerAccordion.htmlCtrl.toggle();
+		this[this.ctrl].toggle();
 	}
 }
 // Inject the power css on PowerUi
