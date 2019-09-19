88dfb846be3868e55dd8f80677d7f172490654ff
André Maia <pitymaia@gmail.com>
2019-09-18 21:28:29 -0300

    Replace eval with something safer

--- source/scripts/power_templating.js
+++ source/scripts/power_templating.js
@@ -45,6 +45,68 @@ class PowerTemplating {
 	getInterpolationValue(entry) {
 		let newEntry = this.stripWhiteChars(entry);
 		newEntry = this.stripInterpolation(newEntry);
-		return eval(newEntry);
+		const func = new Function("return " + newEntry);
+		return this.safeEvaluate(newEntry);
 	}
+
+	// TODO: This is not really safe, just good to use during ALPHA phase of development
+	// remove functions, arrow functions, document.*() and window.*(), script and more
+	sanitizeEntry(entry) {
+		const REGEXLIST = [
+			/function[^]*?\)/gm,
+			/function/gm,
+			/=>[^]*?/gm,
+			/=>/gm,
+			/localStorage\.[^]*?\)/gm,
+			/localStorage/gm,
+			/window\.[^]*?\)/gm,
+			/window/gm,
+			/document\.[^]*?\)/gm,
+			/document/gm,
+			/write/gm,
+			/console\.[^]*?\)/gm,
+			/console/gm,
+			/alert[^]*?\)/gm,
+			/alert/gm,
+			/eval[^]*?\)/gm,
+			/eval/gm,
+			/request/gm,
+			/ajaxRequest/gm,
+			/loadHtmlView/gm,
+			/XMLHttpRequest/gm,
+			/new[^]*?\)/gm,
+			/new/gm,
+			/<[^]*?script[^]*?>[^]*?<[^]*?\/[^]*?script[^]*?>/gm,
+			/script/gm,
+		];
+
+		let newEntry = entry;
+
+		for (const regex of REGEXLIST) {
+			newEntry = newEntry.replace(regex,'');
+		}
+		return this.safeString(newEntry);
+	}
+
+	safeEvaluate(entry) {
+		let func;
+		let result;
+		try {
+			func = new Function("return " + this.sanitizeEntry(entry));
+			result = func();
+		} catch(e) {
+			result = '';
+		}
+		if (result === undefined) {
+			return '';
+		} else {
+			return result;
+		}
+	}
+
+	safeString(content) {
+		const tmp = document.createElement('div');
+		tmp.textContent = content;
+		return tmp.innerHTML;
+	};
 }
