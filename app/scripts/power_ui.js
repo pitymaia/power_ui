44f710bb0bd9180ef2ec8a8625f8f692ed2b1cb8
André Maia <pitymaia@gmail.com>
2019-12-24 04:59:56 -0300

    WIP: Draggable Window

--- app/scripts/power_ui.js
+++ app/scripts/power_ui.js
@@ -5472,7 +5472,11 @@ class PowerWindow extends PowerDialogBase {
 	}
 
 	_onViewLoad() {
-		console.log('!!!!!!!!!!!!1 this', this._viewId);
+		// Make the DIV element draggable:
+		const currentView = document.getElementById(this._viewId);
+		const currentWindow = currentView.getElementsByClassName('pw-dialog-container')[0];
+		console.log('!!!!!!!!!!!! currentWindow', currentWindow);
+		this.dragElement(currentWindow);
 	}
 
 	template({$title}) {
@@ -5481,6 +5485,47 @@ class PowerWindow extends PowerDialogBase {
 		return `<div class="pw-dialog pw-dialog-container">
 					${super.template({$title})}
 				</div>`;
+	}
+
+	dragElement(elmnt) {
+		var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+		if (document.getElementById(elmnt.id + "header")) {
+			// if present, the header is where you move the DIV from:
+			document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+		} else {
+			// otherwise, move the DIV from anywhere inside the DIV:
+			elmnt.onmousedown = dragMouseDown;
+		}
+
+		function dragMouseDown(e) {
+			e = e || window.event;
+			e.preventDefault();
+			// get the mouse cursor position at startup:
+			pos3 = e.clientX;
+			pos4 = e.clientY;
+			document.onmouseup = closeDragElement;
+			// call a function whenever the cursor moves:
+			document.onmousemove = elementDrag;
+		}
+
+		function elementDrag(e) {
+			e = e || window.event;
+			e.preventDefault();
+			// calculate the new cursor position:
+			pos1 = pos3 - e.clientX;
+			pos2 = pos4 - e.clientY;
+			pos3 = e.clientX;
+			pos4 = e.clientY;
+			// set the element's new position:
+			elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+			elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+		}
+
+		function closeDragElement() {
+			// stop moving when mouse button is released:
+			document.onmouseup = null;
+			document.onmousemove = null;
+		}
 	}
 }
 
