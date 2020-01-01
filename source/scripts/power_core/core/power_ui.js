9bdb1eeaf4121d18a1cef425a92ad8ab86f44002
André Maia <pitymaia@gmail.com>
2020-01-01 20:43:08 -0300

    Add domain to setCookie

--- source/scripts/power_core/core/power_ui.js
+++ source/scripts/power_core/core/power_ui.js
@@ -155,14 +155,14 @@ class PowerUi extends _PowerUiBase {
 		return null;
 	}
 
-	setCookie(name, value, days) {
+	setCookie(name, value, days, domain) {
 	    var expires = "";
 	    if (days) {
 	        var date = new Date();
 	        date.setTime(date.getTime() + (days*24*60*60*1000));
-	        expires = "; expires=" + date.toUTCString();
+	        expires = ";expires=" + date.toUTCString();
 	    }
-	    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+	    document.cookie = name + "=" + (value || "")  + expires + `${'; ' + domain || ''}; path=/`;
 	}
 
 	_addService(key, service) {
