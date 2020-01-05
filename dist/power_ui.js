ca96f187cce39f5ba46227ea205a3edb75d28883
André Maia <pitymaia@gmail.com>
2020-01-05 05:26:23 -0300

    Dist files with powerTreeTemplate

--- dist/power_ui.js
+++ dist/power_ui.js
@@ -1467,6 +1467,10 @@ class PowerUi extends _PowerUiBase {
 		document.addEventListener('keyup', this._keyUp.bind(this), false);
 	}
 
+	treeTemplate(tree) {
+		return new PowerTreeTemplate({$powerUi: this, tree: tree}).template;
+	}
+
 	getCookie(name) {
 		const nameEQ = name + "=";
 		const ca = document.cookie.split(';');
@@ -5327,7 +5331,13 @@ class PowerWidget extends PowerController {
 
     $buildTemplate({template, title}) {
         const tempElement = document.createElement('div');
+        if (this.addBeforeTemplate) {
+            template = this.addBeforeTemplate + template;
+        }
         tempElement.innerHTML = this.template({$title: title || null});
+        if (this.addAfterTemplate) {
+            template = template + this.addAfterTemplate;
+        }
         const content = tempElement.querySelectorAll('[data-pw-content]');
         content[0].innerHTML = template;
         template = tempElement.innerHTML;
@@ -5561,6 +5571,37 @@ class PowerModal extends PowerDialogBase {
 }
 
 export { PowerModal };
+
+class PowerTreeTemplate {
+	constructor({$powerUi, tree}) {
+		this.$powerUi = $powerUi;
+		this.tree = tree;
+		this.template = this.buildTemplate(this.tree);
+	}
+
+	buildTemplate(tree, id) {
+		let template = `<nav ${id ? 'id="' + id + '"' : ''} class="power-tree-view">`;
+		for (const item of tree) {
+			if (item.kind === 'file') {
+				template = `${template}
+					<a class="power-item"><span class="power-icon fa fa-file"></span> ${item.fullName}</a>`;
+			} else if (item.kind === 'folder') {
+				const id = `list-${this.$powerUi._Unique.next()}`;
+				template = `${template}
+				<a class="power-list" data-power-target="${id}">
+					<span class="power-status fa" data-power-active="fa-folder-open" data-power-inactive="fa-folder"></span> ${item.fullName}
+				</a>
+				${this.buildTemplate(item.content, id)}`;
+			}
+		}
+		template = `${template}
+		</nav>`;
+
+		return template;
+	}
+}
+
+// export { PowerTreeTemplate };
 
 class PowerWindow extends PowerDialogBase {
 	constructor({$powerUi}) {
