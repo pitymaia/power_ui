903858a26529cd82e7e9c9932ebcbef411ffd022
André Maia <pitymaia@gmail.com>
2020-08-31 20:49:52 -0300

    dist files

--- dist/power_ui.js
+++ dist/power_ui.js
@@ -6079,10 +6079,9 @@ class PowerTemplate extends PowerScope {
 			const self = this;
 			const css = new Promise(this.css.bind(this));
 			css.then(function (response) {
-				self.response = response;
-				self.appendCss();
+				self.appendCss(response);
 			}).catch(function (response) {
-				console.log('catch', response);
+				window.console.log('Error loading css', response);
 			});
 		}
 
@@ -6093,7 +6092,7 @@ class PowerTemplate extends PowerScope {
 		return new Promise(this.template.bind(this));
 	}
 
-	import(filePath) {
+	_import(filePath) {
 		const self = this;
 		return new Promise(function (resolve, reject) {
 			self.$powerUi.request({
@@ -6110,14 +6109,58 @@ class PowerTemplate extends PowerScope {
 		});
 	}
 
+	import(filePaths, concat) {
+		if (typeof filePaths === 'string' || filePaths instanceof String) {
+			return this._import(filePaths);
+		} else if (filePaths.length) {
+			const promises = [];
+			for (const path of filePaths) {
+				promises.push(this._import(path));
+			}
+
+			if (!concat) {
+				return Promise.all(promises);
+			} else {
+				return new Promise(function (resolve, reject) {
+					Promise.all(promises).then(function (response) {
+						try {
+							let result = '';
+							for (const file of response) {
+								if (concat === true) {
+									result = result + file;
+								} else {
+									result = result + concat + file;
+								}
+							}
+							resolve(result);
+						} catch(error) {
+							window.console.log('Error importing files:', error);
+							reject(error);
+						}
+					});
+				});
+			}
+		} else {
+			throw "Error: import expects a string or array";
+		}
+	}
+
 	request(options) {
 		return this.$powerUi.request(options);
 	}
 
-	appendCss() {
+	appendCss(css) {
+		// Concatenate if is an array
+		if (css.length) {
+			let _new = '';
+			for (const _css of css) {
+				_new = _new + _css;
+			}
+			css = _new;
+		}
 		const head = document.getElementsByTagName('head')[0];
 		let style = document.createElement('style');
-		style.innerHTML = this.response;
+		style.innerHTML = css;
 		style.id = '_css' + this._viewId;
 		head.appendChild(style);
 	}
